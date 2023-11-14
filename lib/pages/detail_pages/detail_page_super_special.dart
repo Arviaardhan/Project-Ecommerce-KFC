e72d64c3074c1a4c5379919f07596e449f483b6a
jexpl12 <kolekmemilihmutad@gmail.com>
2023-11-15 00:06:30 +0700

    style : [BW] add bottom-Navbar

--- lib/pages/detail_pages/detail_page_super_special.dart
+++ lib/pages/detail_pages/detail_page_super_special.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:iconify_flutter/icons/heroicons.dart';
 import 'package:iconify_flutter/icons/mdi.dart';
 import 'package:project_ecommerce/pages/order_page.dart';
 import 'package:project_ecommerce/widgets/widget.dart';
 import 'package:project_ecommerce/helper/themes.dart';
 import 'package:project_ecommerce/controllers/kfc_controller.dart';
+import 'package:project_ecommerce/pages/voucher_page.dart';
+import 'package:project_ecommerce/pages/profile_page.dart';
+import 'package:project_ecommerce/pages/home_page.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:get/get.dart';
-
-import '../../models/kfc_model.dart';
+import 'package:project_ecommerce/models/kfc_model.dart';
 
 class SuperSpecial extends StatelessWidget {
   SuperSpecial({Key? key}) : super(key: key);
@@ -87,6 +91,50 @@ class SuperSpecial extends StatelessWidget {
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                Get.to(() => HomePage());
+              },
+              icon: Iconify(Heroicons.home_solid, color: primaryColor),
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: const EdgeInsets.only(right: 30),
+              child: IconButton(
+                onPressed: () {
+                  Get.to(() => OrderPage());
+                },
+                icon: Iconify(Mdi.cart_outline, color: primaryColor),
+              ),
+            ),
+            label: "My Order",
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                Get.to(() => VoucherPage());
+              },
+              icon: Iconify(Mdi.voucher_outline, color: primaryColor),
+            ),
+            label: "Voucher",
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                Get.to(() => ProfilePage());
+              },
+              icon: Iconify(Mdi.user_circle_outline, color: primaryColor),
+            ),
+            label: "Profile",
+          ),
+        ],
+        showSelectedLabels: false,
       ),
     );
   }
