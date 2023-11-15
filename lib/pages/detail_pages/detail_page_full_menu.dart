bb83debb93018a81cb26f7050ab5d8d2181fdae6
jexpl12 <kolekmemilihmutad@gmail.com>
2023-11-15 09:39:12 +0700

    style : [BW] add detail-menu

--- lib/pages/detail_pages/detail_page_full_menu.dart
+++ lib/pages/detail_pages/detail_page_full_menu.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:iconify_flutter/icons/heroicons.dart';
+import 'package:iconify_flutter/icons/mdi.dart';
 import 'package:project_ecommerce/helper/themes.dart';
+import 'package:project_ecommerce/pages/home_page.dart';
+import 'package:project_ecommerce/pages/order_page.dart';
+import 'package:project_ecommerce/pages/profile_page.dart';
+import 'package:project_ecommerce/pages/voucher_page.dart';
 import 'package:project_ecommerce/widgets/widget.dart';
 import 'package:project_ecommerce/controllers/kfc_controller.dart';
 import 'package:shimmer/shimmer.dart';
@@ -7,6 +14,7 @@ import 'package:get/get.dart';
 
 class FullMenu extends StatelessWidget {
   FullMenu({Key? key}) : super(key: key);
+
   final kfcController = Get.put(KfcController());
 
   @override
@@ -45,6 +53,50 @@ class FullMenu extends StatelessWidget {
                   );
                 },
               ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                Get.to(() => HomePage());
+              },
+              icon: Iconify(Heroicons.home_solid, color: primaryColor),
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: const EdgeInsets.only(right: 30),
+              child: IconButton(
+                onPressed: () {
+                  Get.to(() => OrderPage());
+                },
+                icon: Iconify(Mdi.cart_outline, color: primaryColor),
+              ),
+            ),
+            label: "My Order",
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                Get.to(() => VoucherPage());
+              },
+              icon: Iconify(Mdi.voucher_outline, color: primaryColor),
+            ),
+            label: "Voucher",
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                Get.to(() => ProfilePage());
+              },
+              icon: Iconify(Mdi.user_circle_outline, color: primaryColor),
+            ),
+            label: "Profile",
+          ),
+        ],
+        showSelectedLabels: false,
       ),
     );
   }
--- lib/pages/detail_pages/detail_page_super_alacarte.dart
+++ lib/pages/detail_pages/detail_page_super_alacarte.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:iconify_flutter/icons/mdi.dart';
+import 'package:project_ecommerce/pages/detail_pages/detail_menu.dart';
 import 'package:project_ecommerce/pages/home_page.dart';
 import 'package:project_ecommerce/pages/order_page.dart';
 import 'package:project_ecommerce/pages/profile_page.dart';
@@ -10,6 +11,7 @@ import 'package:project_ecommerce/helper/themes.dart';
 import 'package:project_ecommerce/controllers/kfc_controller.dart';
 import 'package:get/get.dart';
 import 'package:project_ecommerce/models/kfc_model.dart';
+
 
 class SuperAlacarte extends StatelessWidget {
   SuperAlacarte({Key? key}) : super(key: key);
@@ -30,56 +32,71 @@ class SuperAlacarte extends StatelessWidget {
                     .length,
                 itemBuilder: (BuildContext context, int index) {
                   final kfc = kfcController.kfcresponsemodel[0];
-                  final comboItems = kfc.menu
+                  final alacarteItems = kfc.menu
                       .where((item) => item.category == Category.ALACARTE)
                       .toList();
-                  final menuItem = comboItems[index];
+                  final menuItem = alacarteItems[index];
                   final harga = menuItem.price;
-                  return Container(
-                    margin: EdgeInsets.only(top: 10, bottom: 10),
-                    width: 370,
-                    height: 160,
-                    child: Card(
-                      margin: EdgeInsets.only(left: 15, right: 15),
-                      elevation: 4,
-                      shadowColor: Colors.grey,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: ListTile(
-                        contentPadding: EdgeInsets.only(top: 17,),
-                        leading: Container(
-                          margin: EdgeInsets.only(left: 30, right: 20),
-                          child: Image.network(
-                            menuItem.image,
-                            width: 80,
-                            height: 80,
-                            fit: BoxFit.cover,
-                          ),
+                  return GestureDetector(
+                    onTap: () {
+                      // Navigate to the detail menu page
+                      Get.to(() => DetailMenuPage(menuItem: menuItem));
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(top: 10, bottom: 10),
+                      width: 370,
+                      height: 160,
+                      child: Card(
+                        margin: EdgeInsets.only(left: 15, right: 15),
+                        elevation: 4,
+                        shadowColor: Colors.grey,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
                         ),
-                        title: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              menuItem.name,
-                              style: namePriceMenu,
+                        child: ListTile(
+                          contentPadding: EdgeInsets.only(
+                            top: 17,
+                          ),
+                          leading: Container(
+                            margin: EdgeInsets.only(left: 30, right: 20),
+                            child: Image.network(
+                              menuItem.image,
+                              width: 80,
+                              height: 80,
+                              fit: BoxFit.cover,
                             ),
-                            SizedBox(height: 10),
-                            for (var foodItem in menuItem.food)
-                              Text(foodItem, style: foodMenu,),
-                            SizedBox(height: 10),
-                            Row(
-                              children: [
+                          ),
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                menuItem.name,
+                                style: namePriceMenu,
+                              ),
+                              SizedBox(height: 10),
+                              for (var foodItem in menuItem.food)
                                 Text(
-                                  "Rp. $harga", style: namePriceMenu,
+                                  foodItem,
+                                  style: foodMenu,
                                 ),
-                                Padding(
-                                  padding: EdgeInsets.only(left: 40, ),
-                                  child: myAddButton(context, "Add", Mdi.cart_add, kfcController, index),
-                                )
-                              ],
-                            ),
-                          ],
+                              SizedBox(height: 10),
+                              Row(
+                                children: [
+                                  Text(
+                                    "Rp. $harga",
+                                    style: namePriceMenu,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(
+                                      left: 40,
+                                    ),
+                                    child: myAddButton(context, "Add",
+                                        Mdi.cart_add, kfcController, index),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
