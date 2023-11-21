f49c15bdfdd522876ef0936e3ee37483406bdf16
Arviaardhan <arviaardhan@gmail.com>
2023-11-21 21:22:07 +0700

    style : [AA] change-style-voucherpage

--- lib/pages/voucher_page.dart
+++ lib/pages/voucher_page.dart
@@ -4,12 +4,9 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:iconify_flutter/icons/heroicons.dart';
 import 'package:iconify_flutter/icons/mdi.dart';
-import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:project_ecommerce/pages/profile_page.dart';
-import 'package:project_ecommerce/models/voucher_model.dart';
 import 'package:project_ecommerce/controllers/voucher_controller.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 import '../helper/themes.dart';
 import 'home_page.dart';
@@ -20,195 +17,162 @@ class VoucherPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 50,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border(
-                left: BorderSide(
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.fromBorderSide(BorderSide(
                   color: Colors.black.withOpacity(0.1),
-                ),
-                top: BorderSide(
-                  color: Colors.black.withOpacity(0.1),
-                ),
-                right: BorderSide(
-                  color: Colors.black.withOpacity(0.1),
-                ),
-                bottom: BorderSide(
                   width: 1,
-                  color: Colors.black.withOpacity(0.1),
-                ),
+                )),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x3F000000),
+                    blurRadius: 4,
+                    offset: Offset(0, 1),
+                    spreadRadius: 0,
+                  ),
+                ],
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0x3F000000),
-                  blurRadius: 4,
-                  offset: Offset(0, 1),
-                  spreadRadius: 0,
-                ),
-              ],
-            ),
-            child: Column(
-              children: [
-                Transform(
-                  transform: Matrix4.identity()
-                    ..translate(0.0, 0.0)
-                    ..rotateZ(-1.57),
-                  child: Container(
-                    width: 30,
-                    height: 30,
-                    clipBehavior: Clip.antiAlias,
-                    decoration: BoxDecoration(),
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          left: 1.25,
-                          top: 1.25,
-                          child: Container(
-                            width: 27.50,
-                            height: 27.50,
-                            child: Stack(
-                              children: [
-                                // Add your voucher icon here
-                                Icon(Icons.arrow_back, color: Colors.red)
-                              ],
+              child: Column(
+                children: [
+                  Transform.rotate(
+                    angle: -1.57,
+                    child: Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 1.25,
+                            top: 1.25,
+                            child: Container(
+                              width: 27.50,
+                              height: 27.50,
+
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Center(
-                  child: Text(
-                    'List Voucher',
-                    style: TextStyle(
-                      color: Color(0xFFE7002B),
-                      fontSize: 24,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w600,
-                      height: 0.03,
+                  Center(
+                    child: Text(
+                      'List Voucher',
+                      style: TextStyle(
+                        color: Color(0xFFE7002B),
+                        fontSize: 24,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                        height: 0.03,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Obx(
-            () {
-              if (_voucherController.vouchers.isEmpty) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else {
-                return Expanded(
-                  child: ListView.builder(
-                    itemCount: _voucherController.vouchers.length,
-                    itemBuilder: (context, index) {
-                      final voucher = _voucherController.vouchers[index];
-                      return Container(
-                        margin: EdgeInsets.only(top: 13),
-                        width: 360,
-                        height: 161,
-                        child: Stack(
+            Obx(() => _voucherController.vouchers.isEmpty
+                  ? Center(child: CircularProgressIndicator())
+                  : Expanded(child: ListView.builder(
+                  itemCount: _voucherController.vouchers.length,
+                  itemBuilder: (context, index) {
+                    final voucher = _voucherController.vouchers[index];
+                    return Center(
+                      child: Container(
+                        margin: EdgeInsets.only(top: 10, bottom: 20),
+                        width: screenWidth * 0.9,
+                        height: screenHeight * 0.2,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0x3F000000),
+                              blurRadius: 8,
+                              offset: Offset(4, 4),
+                              spreadRadius: 0,
+                            ),
+                          ],
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            Positioned(
-                              left: 0,
-                              top: 0,
+                            Padding(
+                              padding: EdgeInsets.only(left: 25),
                               child: Container(
-                                width: 360,
+                                width: 180,
                                 height: 161,
                                 decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(20),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Color(0x3F000000),
-                                      blurRadius: 8,
-                                      offset: Offset(4, 4),
-                                      spreadRadius: 0,
-                                    ),
-                                  ],
+                                  image: DecorationImage(
+                                    image: NetworkImage(voucher.image),
+                                    fit: BoxFit.cover,
+                                  ),
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(20),
+                                    bottomLeft: Radius.circular(20),
+                                  ),
                                 ),
-                                child: Stack(
+                              ),
+                            ),
+                            SizedBox(width: 20),
+                            Container(
+                              width: 160,
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 10, top: 15),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
-                                    Positioned(
-                                      left: 19,
-                                      top: 9,
-                                      child: Container(
-                                        width: 158,
-                                        height: 143,
-                                        decoration: ShapeDecoration(
-                                          image: DecorationImage(
-                                            image: NetworkImage(voucher.image),
-                                            fit: BoxFit.cover,
-                                          ),
-                                          shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                          ),
-                                        ),
+                                    Text(
+                                      voucher.name,
+                                      style: TextStyle(
+                                        color: Color(0xFF252525),
+                                        fontSize: 22,
+                                        fontFamily: 'Poppins',
+                                        fontWeight: FontWeight.w600,
+                                        height: 1.5,
                                       ),
                                     ),
-                                    Positioned(
-                                      left: 198,
-                                      top: 28,
-                                      child: Container(
-                                        width: 160,
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              voucher.name,
-                                              style: TextStyle(
-                                                color: Color(0xFF252525),
-                                                fontSize: 22,
-                                                fontFamily: 'Poppins',
-                                                fontWeight: FontWeight.w600,
-                                                height: 1.5,
-                                              ),
-                                            ),
-                                            SizedBox(height: 8),
-                                            Row(
-                                              children: [
-                                                Iconify(Mdi.clock,
-                                                    color: Color(0xFFE7002B),
-                                                    size: 15),
-                                                SizedBox(width: 4),
-                                                Text(
-                                                  voucher.limit,
-                                                  style: TextStyle(
-                                                    color: Color(0xFF4D4C4C),
-                                                    fontSize: 10,
-                                                    fontFamily: 'Poppins',
-                                                    fontWeight: FontWeight.w600,
-                                                    height: 0.30,
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          ],
+                                    SizedBox(height: 8),
+                                    Row(
+                                      children: [
+                                        Iconify(Mdi.clock,
+                                            color: Color(0xFFE7002B),
+                                            size: 15),
+                                        SizedBox(width: 4),
+                                        Text(
+                                          voucher.limit,
+                                          style: TextStyle(
+                                            color: Color(0xFF4D4C4C),
+                                            fontSize: 10,
+                                            fontFamily: 'Poppins',
+                                            fontWeight: FontWeight.w600,
+                                            height: 0.30,
+                                          ),
                                         ),
-                                      ),
-                                    )
+                                      ],
+                                    ),
                                   ],
                                 ),
                               ),
                             ),
                           ],
                         ),
-                      );
-                    },
-                  ),
-                );
-              }
-            },
-          ),
-        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
