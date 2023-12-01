a0875db9fed600e8f993687b3c97aa505329e396
jexpl12 <kolekmemilihmutad@gmail.com>
2023-12-01 21:52:31 +0700

    style : [BW] add profile

---
+++ lib/models/profile_model.dart
@@ -0,0 +1,11 @@
+class UserProfile {
+  final String username;
+  final String fullName;
+  final String email;
+
+  UserProfile({
+    required this.username,
+    required this.fullName,
+    required this.email,
+  });
+}
